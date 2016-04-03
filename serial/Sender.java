bfd136bf87bfcf0f48bb612032807d2878e9cc60
gordonhart <gordon.hart2@gmail.com>
2016-04-03 18:17:53 -0400

    Wrote own serial communicator in java

---
+++ serial/Sender.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+
+import java.util.*;
+import jssc.*; 
+
+
+public class Sender {
+	private static SerialPort serialPort;
+
+
+	public static void main(String[] args) {
+		if(args.length == 0) {
+			System.out.printf("Call with argument: java -cp \"jssc-2.8.0.jar\" Sender <cmd>");
+		} else {
+			// System.out.printf("argv[0] : %s\n", args[0]);
+			connect("/dev/tty.usbserial-DA00866A");
+			sendGCode(args[0]+"\n");
+			disconnect();
+		}
+	}
+
+
+	private static void connect(String port){
+		serialPort = new SerialPort(port);
+
+		try{
+			serialPort.openPort();
+
+			serialPort.setParams(SerialPort.BAUDRATE_115200,
+					SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+
+			serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_XONXOFF_IN | SerialPort.FLOWCONTROL_XONXOFF_OUT);
+
+			serialPort.addEventListener(new PortListener(), SerialPort.MASK_RXCHAR);
+		}
+		catch (SerialPortException ex) {
+			System.out.println("Couldn't open port: " + ex);
+		}	
+	}
+
+	private static void disconnect() {
+		try{
+			sendGCode("$md\n"); //kill motors
+			serialPort.closePort();
+		} catch (SerialPortException ex) {
+			System.out.println("Couldn't close port: " + ex);
+		}
+	}
+
+	public static void sendGCode(String gCode){
+		try{
+			serialPort.writeString(gCode);
+		} catch (SerialPortException ex) {
+			System.out.println("Couldn't write to port: " + ex);
+		}
+	}
+
+
+	private static class PortListener implements SerialPortEventListener {
+
+		public void serialEvent(SerialPortEvent event) {
+			if(event.isRXCHAR() && event.getEventValue() > 0) {
+				try {
+					String receivedData = serialPort.readString(event.getEventValue());
+					// System.out.println(receivedData);
+				}
+				catch (SerialPortException ex) {
+					System.out.println("Error in receiving string from COM-port: " + ex);
+				}
+			}
+		}
+	}
+}
